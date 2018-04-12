9fd4993998bb8e7ff9820c0b57a2e33783d4939a
Abhishek <abhidhotre91@yahoo.com>
2018-04-12 02:32:23 -0500

    Exchange fragment code

--- app/src/main/java/com/example/cse6324/university_bazaar_system/ExchangeFragment.java
+++ app/src/main/java/com/example/cse6324/university_bazaar_system/ExchangeFragment.java
@@ -1,12 +1,30 @@
 package com.example.cse6324.university_bazaar_system;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -22,10 +40,15 @@ public class ExchangeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int PICK_IMAGE_REQUEST = 71;
+
+    private Uri filePath;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private FirebaseStorage storage;
 
     private OnFragmentInteractionListener mListener;
 
@@ -54,6 +77,7 @@ public class ExchangeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        storage = FirebaseStorage.getInstance();
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -64,7 +88,64 @@ public class ExchangeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exchange, container, false);
+        final View myFragmentView = inflater.inflate(R.layout.fragment_exchange, container, false);
+        Button btnChoose = myFragmentView.findViewById(R.id.btnExchangeImage);
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        }) ;
+
+        Button btnPost = myFragmentView.findViewById(R.id.btnExchange);
+        final EditText etName = myFragmentView.findViewById(R.id.etExchangeName);
+        final EditText etDesc = myFragmentView.findViewById(R.id.etExchangeDesc);
+        final EditText etCriteria = myFragmentView.findViewById(R.id.etExchangeCriteria);
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+                final String str = sdf.format(Calendar.getInstance().getTime()).toString();
+                StorageReference storageRef = storage.getReference();
+                StorageReference riversRef = storageRef.child("images/"+ str + ".jpg");
+                UploadTask uploadTask = riversRef.putFile(filePath);
+
+                // Register observers to listen for when the download is done or if it fails
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                        Map<String, Object> item = new HashMap<>();
+                        item.put("etName", etName.getText().toString());
+                        item.put("etDesc", etDesc.getText().toString());
+                        item.put("etPrice", "");
+                        item.put("etDuration", "");
+                        item.put("etLocation", "");
+                        item.put("etExchangeCriteria", etCriteria.getText().toString());
+                        item.put("imgPath",str + ".jpg");
+                        item.put("etType", "Exchange");
+
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                        db.collection("items").document(str).set(item);
+
+                        Toast.makeText(getContext(), "Merchandise listed for Exchange", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        return myFragmentView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -74,7 +155,7 @@ public class ExchangeFragment extends Fragment {
         }
     }
 
-    @Override
+    /*@Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
@@ -89,6 +170,23 @@ public class ExchangeFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }*/
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            /*try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }*/
+        }
     }
 
     /**
